4cdbc9683efe860ec40b6dd01da9a27099bea3bd
Haedar15 <frsdafa123@gmail.com>
2025-12-14 14:59:31 +0700

    Revert "supabase auth setup"
    This reverts commit fef2e40ed233fc796a1edc438a9401ac758712e2.

--- app/src/main/java/com/papb/projectakhirandroid/di/RepositoryModule.kt
+++ app/src/main/java/com/papb/projectakhirandroid/di/RepositoryModule.kt
@@ -1,10 +1,27 @@
 package com.papb.projectakhirandroid.di
 
-import com.papb.projectakhirandroid.data.repository.AuthRepository
+import android.content.Context
+import com.papb.projectakhirandroid.data.local.ProductDatabase
+import com.papb.projectakhirandroid.data.repository.LocalDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import com.papb.projectakhirandroid.data.repository.OnBoardingOperationImpl
+import com.papb.projectakhirandroid.data.repository.Repository
+import com.papb.projectakhirandroid.domain.repository.LocalDataSource
+import com.papb.projectakhirandroid.domain.repository.OnBoardingOperation
+import com.papb.projectakhirandroid.domain.usecase.UseCases
+import com.papb.projectakhirandroid.domain.usecase.addcartusecase.AddCartUseCase
+import com.papb.projectakhirandroid.domain.usecase.deletecartusecase.DeleteCartUseCase
+import com.papb.projectakhirandroid.domain.usecase.getallcartusecase.GetAllCartUseCase
+import com.papb.projectakhirandroid.domain.usecase.getallproduct.GetAllProductUseCase
+import com.papb.projectakhirandroid.domain.usecase.getselectedproduct.GetSelectedProductUseCase
+import com.papb.projectakhirandroid.domain.usecase.readonboarding.ReadOnBoardingUseCase
+import com.papb.projectakhirandroid.domain.usecase.saveonboarding.SaveOnBoardingUseCase
+import com.papb.projectakhirandroid.domain.usecase.saveproductusecase.InsertProductsUseCase
+import com.papb.projectakhirandroid.domain.usecase.searchproductusecase.SearchProductUseCase
 import javax.inject.Singleton
 
 @Module
@@ -13,7 +30,30 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideAuthRepository(): AuthRepository {
-        return AuthRepository()
+    fun provideDataStoreOperation(
+        @ApplicationContext context: Context
+    ): OnBoardingOperation = OnBoardingOperationImpl(context = context)
+
+    @Provides
+    @Singleton
+    fun provideLocalDataSource(productDatabase: ProductDatabase): LocalDataSource {
+        return LocalDataSourceImpl(productDatabase)
     }
+
+    @Provides
+    @Singleton
+    fun provideUseCase(repository: Repository): UseCases {
+        return UseCases(
+            saveOnBoardingUseCase = SaveOnBoardingUseCase(repository),
+            insertProductsUseCase = InsertProductsUseCase(repository),
+            readOnBoardingUseCase = ReadOnBoardingUseCase(repository),
+            getSelectedProductUseCase = GetSelectedProductUseCase(repository),
+            getAllProductUseCase = GetAllProductUseCase(repository),
+            getAllCartUseCase = GetAllCartUseCase(repository),
+            addCartUseCase = AddCartUseCase(repository),
+            deleteCart = DeleteCartUseCase(repository),
+            searchProductUseCase = SearchProductUseCase(repository)
+        )
+    }
+
 }
